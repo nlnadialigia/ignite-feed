62aaa06b1d44cd6dfaf82a3ba4c9b6ed7220ba38
Nadia Ligia <nlnadialigia@gmail.com>
2022-06-11 20:45:46 -0300

    style($style): create global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
-import './styles.css';
+import './global.css';
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Nádia Ligia"
         content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum eos id ex aut, ratione quos officia natus corrupti, saepe perferendis sint? Voluptatum, ipsam? Ratione sint nemo laborum aperiam expedita"
