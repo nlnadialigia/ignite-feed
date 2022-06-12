c79a14737a18fb8c76d6d43ef343c2d8b759922b
Nadia Ligia <nlnadialigia@gmail.com>
2022-06-11 21:16:04 -0300

    feat($app): create main content and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
 import './global.css';
+import styles from './App.module.css';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Nádia Ligia"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum eos id ex aut, ratione quos officia natus corrupti, saepe perferendis sint? Voluptatum, ipsam? Ratione sint nemo laborum aperiam expedita"
-      />
-      <Post
-        author="Joana D'arc"
-        content="Nulla quis lorem ut libero malesuada feugiat."
-      />
+
+      <div className={styles.wrapper}>
+        <aside>Sidebar</aside>
+        <main>
+          <Post
+            author="Nádia Ligia"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi rerum eos id ex aut, ratione quos officia natus corrupti, saepe perferendis sint? Voluptatum, ipsam? Ratione sint nemo laborum aperiam expedita"
+          />
+          <Post
+            author="Joana D'arc"
+            content="Nulla quis lorem ut libero malesuada feugiat."
+          />
+        </main>
+      </div>
     </div>
   );
 }
