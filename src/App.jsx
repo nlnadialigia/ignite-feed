3f5b1daae3656c43fb26d2a74527796ef9855d02
Nadia <nlnadialigia@gmail.com>
2024-02-21 09:51:06 -0300

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { Header } from "./components/Header/Header"
-import { Post } from "./Post"
-import "./global.css"
 import styles from "./App.module.css"
+import { Header } from "./components/Header/Header"
+import { Post } from "./components/Post/Post"
 import { Sidebar } from "./components/Sidebar/Sidebar"
+import "./global.css"
 
 export function App() {
   return (
