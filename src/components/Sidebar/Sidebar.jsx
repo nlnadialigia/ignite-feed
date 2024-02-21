98fc23d5be16d2a8f83f28e5ecf3c17095c0f399
Nadia <nlnadialigia@gmail.com>
2024-02-21 10:32:17 -0300

    start sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,18 @@
+import styles from "./Sidebar.module.css";
+
 export function Sidebar() {
   return (
-    <div>
-      <h1>Sidebar</h1>
-    </div>
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src="https://images.unsplash.com/photo-1488866022504-f2584929ca5f?q=50&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="User choice" />
+      
+      <div className={styles.profile}>
+        <strong>Nadia Ligia</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
   )
 }
