ecaa965340a566fa66c6e7cf50b80cd89e9e5d61
Nadia <nlnadialigia@gmail.com>
2024-02-23 08:47:36 -0300

    include Sidebar styles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,8 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1488866022504-f2584929ca5f?q=50&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="User choice" />
       
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/nlnadialigia.png" alt="Avatar" />
+        
         <strong>Nadia Ligia</strong>
         <span>Web Developer</span>
       </div>
